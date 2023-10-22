9c8ee5412b51238d704929325636d93a01808ee1
Reynaldo <reynaldommaciel@gmail.com>
2023-10-22 07:56:42 -0300

    Create main, list of meals and detail screen

---
+++ lib/widgets/category_grid_item.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+
+import 'package:flutter_meals_app/models/category.dart';
+
+class CategoryGridItem extends StatelessWidget {
+  const CategoryGridItem(
+      {super.key, required this.category, required this.onSelectCategory});
+
+  final Category category;
+  final void Function(BuildContext context, Category category) onSelectCategory;
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      onTap: () {
+        onSelectCategory(context, category);
+      },
+      splashColor: Theme.of(context).primaryColor,
+      borderRadius: BorderRadius.circular(16),
+      child: Container(
+        padding: const EdgeInsets.all(12),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(16),
+          gradient: LinearGradient(
+            colors: [
+              category.color.withOpacity(0.55),
+              category.color.withOpacity(0.9),
+            ],
+          ),
+        ),
+        child: Text(
+          category.title,
+          style: Theme.of(context)
+              .textTheme
+              .titleLarge!
+              .copyWith(color: Theme.of(context).colorScheme.onBackground),
+        ),
+      ),
+    );
+  }
+}
